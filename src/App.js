fab18b355fe7f8a4d6d78f6111a9ccd615640c37
천명진 <cjsaudwls12@gmail.com>
2023-09-08 21:13:10 +0900

    풀페이지 포트폴리오

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
+
+import Dots from './Dots';
+
 import './App.css';
 
+const DIVIDER_HEIGHT = 5;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const outerDivRef = useRef();
+    const [scrollIndex, setScrollIndex] = useState(1);
+    useEffect(() => {
+        const wheelHandler = (e) => {
+            e.preventDefault();
+            const { deltaY } = e;
+            const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+            const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+
+            if (deltaY > 0) {
+                // 스크롤 내릴 때
+                if (scrollTop >= 0 && scrollTop < pageHeight) {
+                    //현재 1페이지
+                    console.log('현재 1페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight + DIVIDER_HEIGHT,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(2);
+                } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+                    //현재 2페이지
+                    console.log('현재 2페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(3);
+                } else {
+                    // 현재 3페이지
+                    console.log('현재 3페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(3);
+                }
+            } else {
+                // 스크롤 올릴 때
+                if (scrollTop >= 0 && scrollTop < pageHeight) {
+                    //현재 1페이지
+                    console.log('현재 1페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(1);
+                } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+                    //현재 2페이지
+                    console.log('현재 2페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(1);
+                } else {
+                    // 현재 3페이지
+                    console.log('현재 3페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight + DIVIDER_HEIGHT,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(2);
+                }
+            }
+        };
+        const outerDivRefCurrent = outerDivRef.current;
+        outerDivRefCurrent.addEventListener('wheel', wheelHandler);
+        return () => {
+            outerDivRefCurrent.removeEventListener('wheel', wheelHandler);
+        };
+    }, []);
+    return (
+        <div ref={outerDivRef} className="outer">
+            <Dots scrollIndex={scrollIndex} />
+            <div className="inner bg-yellow">
+                <div className="Main">
+                    <header className="Main-header">
+                        <div className="Main-spin">Welcome to my portfolio</div>
+                        <p>프론트엔드 개발자 천명진입니다.</p>
+                    </header>
+                    <div id="scroll_down">
+                        <div id="mouse"></div>
+                        <div id="scroll_txt"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="divider"></div>
+            <div className="inner bg-blue">2</div>
+            <div className="divider"></div>
+            <div className="inner bg-pink">3</div>
+        </div>
+    );
 }
 
 export default App;
