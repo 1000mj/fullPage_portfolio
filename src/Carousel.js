5f7b9495bb8b063ea02995a5c8f0b35c25ec4131
천명진 <cjsaudwls12@gmail.com>
2023-11-04 16:47:44 +0900

    캐러셀 완

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.css'; // 스타일링을 위한 CSS 파일
 
 const Carousel = () => {
     const slides = [
-        'Slide 1',
-        'Slide 2',
-        'Slide 3',
-        'Slide 4',
-        // 필요한 만큼 슬라이드 추가
+        'https://via.placeholder.com/600x400.png',
+        'https://via.placeholder.com/600x400/FF0000/FFFFFF.png',
+        'https://via.placeholder.com/600x400/0000FF/FFFFFF.png',
+        'https://via.placeholder.com/600x400/00FF00/FFFFFF.png',
+        // 추가 이미지 경로 추가
     ];
 
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -25,12 +25,14 @@ const Carousel = () => {
             <div className="slides">
                 {slides.map((slide, index) => (
                     <div key={index} className={index === currentSlide ? 'slide active' : 'slide'}>
-                        <a img="https://i.ibb.co/QMXmgj5/python.png"></a>
+                        <img src={slide} alt={`Slide ${index + 1}`} />
                     </div>
                 ))}
             </div>
-            <button onClick={prevSlide}>이전</button>
-            <button onClick={nextSlide}>다음</button>
+            <div className="button-container">
+                <button onClick={prevSlide}>◀</button>
+                <button onClick={nextSlide}>▶</button>
+            </div>
         </div>
     );
 };
