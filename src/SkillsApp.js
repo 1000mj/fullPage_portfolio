9ccd01c32c47bf65fa04827eff93ca1befa1d766
천명진 <cjsaudwls12@gmail.com>
2024-02-25 20:51:26 +0900

    skill 이미지추가 , 정렬

--- src/SkillsApp.js
+++ src/SkillsApp.js
@@ -29,35 +29,35 @@ function SkillsApp() {
             {selectedSkill && (
                 <div className="skill-element">
                     {selectedSkill === 'front-end' && (
-                        <p>
+                        <p className="skills-img">
                             <img src="https://i.ibb.co/NKkTMyk/html.png" alt="" border="0" />
                             <img src="https://i.ibb.co/wBHGJvq/css.png" alt="" border="0" />
                             <img src="https://i.ibb.co/3CZGGQw/javascript.png" alt="javascript" border="0" />
                             <img src="https://i.ibb.co/b2hV55B/react.png" alt="react" border="0" />
-                            <br />
+
                             <img src="https://i.ibb.co/3SM58qq/jquery.png" alt="jquery" border="0" />
                             <img src="https://i.ibb.co/RTCq2C0/bootstrap.png" alt="bootstrap" border="0" />
                             <img src="https://i.ibb.co/3yCYTtV/tailwindcss.png" alt="tailwindcss" border="0" />
                         </p>
                     )}
                     {selectedSkill === 'learning' && (
-                        <p>
-                            :TypeScript
+                        <p className="skills-img">
+                            <img src="https://i.ibb.co/TkpPdfQ/typescript.png" alt="typescript" border="0" />
                             <br />
-                            :Redux
+                            <img src="https://i.ibb.co/86wrS49/reduxx.png" alt="reduxx" border="0" />
                             <br />
-                            :Java
+                            <img src="https://i.ibb.co/j4Pdz8K/java.png" alt="java" border="0" />
                             <br />
-                            :Next.js
+                            <img src="https://i.ibb.co/Gpj1JLZ/NEXT-js.png" alt="NEXT-js" border="0" />
                             <br />
-                            :Python
+                            <img src="https://i.ibb.co/QMXmgj5/python.png" alt="python" border="0" />
                         </p>
                     )}
                     {selectedSkill === 'etc' && (
-                        <p>
-                            :Github
+                        <p className="skills-img">
+                            <img src="https://i.ibb.co/W6r8gYP/github.png" alt="github" border="0" />
                             <br />
-                            :Git
+                            <img src="https://i.ibb.co/s39XCLf/git.png" alt="git" border="0" />
                         </p>
                     )}
                 </div>
